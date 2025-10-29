34d068941e57a17eaee18e838e5cca10021c9a5f
David França <davidemanuel.cf@gmail.com>
2025-10-28 22:22:20 -0300

    feat(core): implement global exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { EnvConfig } from './config/configuration';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,8 @@ async function bootstrap() {
   const env = {
     PORT: configService.get<number>('PORT'),
   } as EnvConfig;
+
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(env.PORT);
 }
