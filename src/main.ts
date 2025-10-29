ca3ec815bbd9a4b37fcc0ecdf67e2d0df9c2c466
David França <davidemanuel.cf@gmail.com>
2025-10-28 22:23:34 -0300

    refactor(main): enable cors and global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { EnvConfig } from './config/configuration';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +14,15 @@ async function bootstrap() {
     PORT: configService.get<number>('PORT'),
   } as EnvConfig;
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.enableCors();
 
   await app.listen(env.PORT);
 }
