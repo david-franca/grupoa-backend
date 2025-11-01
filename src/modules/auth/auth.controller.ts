9ac6c02d3c528cc35cdaa85818e5afff0d049b24
David França <davidemanuel.cf@gmail.com>
2025-11-01 12:29:37 -0300

    refact: add decorators to controllerss

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,7 +2,8 @@ import { Body, Controller, Post, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
+import { ApiLogin, ApiRegister } from '../../common/decorators/auth.decorators';
 
 @ApiTags('1. Autenticação')
 @Controller('auth')
@@ -10,47 +11,13 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  @ApiOperation({ summary: 'Realiza o login e retorna um token JWT' })
-  @ApiResponse({
-    status: 200,
-    description: 'Login bem-sucedido',
-    example: {
-      access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpX...',
-    },
-  })
-  @ApiResponse({
-    status: 401,
-    description: 'Credenciais inválidas',
-    example: {
-      statusCode: 401,
-      timestamp: '2025-10-29T18:54:57.819Z',
-      path: '/auth/login',
-      errors: ['Credenciais inválidas'],
-    },
-  })
+  @ApiLogin()
   async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
 
   @Post('register')
-  @ApiOperation({ summary: 'Registra um novo usuário e retorna um token JWT' })
-  @ApiResponse({
-    status: 201,
-    description: 'Registro bem-sucedido',
-    example: {
-      access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpX...',
-    },
-  })
-  @ApiResponse({
-    status: 400,
-    description: 'Dados de registro inválidos',
-    example: {
-      statusCode: 409,
-      timestamp: '2025-10-29T18:56:35.547Z',
-      path: '/auth/register',
-      errors: ["Um usuário com o e-mail 'email@teste.com.br' já existe."],
-    },
-  })
+  @ApiRegister()
   async register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
   }
--- src/modules/students/students.controller.ts
+++ src/modules/students/students.controller.ts
@@ -17,27 +17,20 @@ import { StudentsService } from './students.service';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import {
-  ApiBearerAuth,
-  ApiOperation,
-  ApiQuery,
-  ApiResponse,
-  ApiTags,
-} from '@nestjs/swagger';
 import { ParamsStudent } from './dto/params-student.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreateStudent,
+  ApiFindAllStudents,
+  ApiFindOneStudent,
+  ApiRemoveStudent,
+  ApiUpdateStudent,
+} from '../../common/decorators/student.decorators';
+import { ApiUnauthorized } from 'src/common/decorators/common.decorators';
 
 @ApiTags('2. Alunos')
 @ApiBearerAuth('JWT-auth')
-@ApiResponse({
-  status: 401,
-  description: 'Não autorizado.',
-  example: {
-    statusCode: 401,
-    timestamp: '2025-10-29T19:07:17.849Z',
-    path: '/students',
-    errors: ['Unauthorized'],
-  },
-})
+@ApiUnauthorized('students')
 @UseGuards(JwtAuthGuard)
 @Controller('students')
 export class StudentsController {
@@ -45,98 +38,18 @@ export class StudentsController {
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  @ApiOperation({ summary: 'Cria um novo aluno' })
-  @ApiResponse({
-    status: 201,
-    description: 'Aluno criado com sucesso.',
-    example: {
-      ra: '26598',
-      cpf: '12345678958',
-      name: 'João da Silva',
-      email: 'jsilva@escola.com.br',
-      created_at: '2025-10-29T01:06:01.783Z',
-      updated_at: '2025-10-29T01:06:01.783Z',
-    },
-  })
-  @ApiResponse({
-    status: 409,
-    description: 'RA ou CPF já existe.',
-    example: {
-      statusCode: 409,
-      timestamp: '2025-10-29T19:08:57.317Z',
-      path: '/students',
-      errors: ["Um aluno com o RA '12345' já existe."],
-    },
-  })
+  @ApiCreateStudent()
   create(@Body() createStudentDto: CreateStudentDto) {
     return this.studentsService.create(createStudentDto);
   }
 
   @Get()
-  @ApiOperation({ summary: 'Retorna todos os alunos paginados' })
-  @ApiResponse({
-    status: 200,
-    description: 'Lista de alunos paginada retornada com sucesso.',
-    example: {
-      items: [
-        {
-          ra: '6597',
-          cpf: '12345678985',
-          name: 'João de Deus',
-          email: 'jdeus@escola.com.br',
-          created_at: '2025-10-28T23:50:40.550Z',
-          updated_at: '2025-10-28T23:50:40.550Z',
-        },
-        {
-          ra: '6598',
-          cpf: '12345678965',
-          name: 'Firmino da Silva',
-          email: 'fsilva@escola.com.br',
-          created_at: '2025-10-29T22:08:43.504Z',
-          updated_at: '2025-10-29T22:08:43.504Z',
-        },
-      ],
-      meta: {
-        totalItems: 251,
-        itemCount: 10,
-        itemsPerPage: 10,
-        totalPages: 26,
-        currentPage: 1,
-      },
-      links: {
-        first: '/students?limit=10',
-        previous: '',
-        next: '/students?page=2&limit=10',
-        last: '/students?page=26&limit=10',
-      },
-    },
-  })
-  @ApiQuery({
-    name: 'search',
-    required: false,
-    type: String,
-    description: 'Busca por RA, CPF, nome ou email',
-  })
-  @ApiQuery({
-    name: 'page',
-    required: false,
-    default: 1,
-    type: Number,
-    description: 'Número da página',
-  })
-  @ApiQuery({
-    name: 'limit',
-    required: false,
-    default: 10,
-    type: Number,
-    description: 'Quantidade de itens por página',
-  })
+  @ApiFindAllStudents()
   findAll(
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number = 1,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number = 10,
     @Query() { search }: ParamsStudent,
   ) {
-    console.log('🚀 ~ StudentsController ~ findAll ~ search:', search);
     return this.studentsService.findAll(
       { limit, page, route: '/students' },
       search,
@@ -144,75 +57,20 @@ export class StudentsController {
   }
 
   @Get(':ra')
-  @ApiOperation({ summary: 'Retorna um aluno pelo RA' })
-  @ApiResponse({
-    status: 200,
-    description: 'Aluno encontrado.',
-    example: {
-      ra: '6597',
-      cpf: '12345678985',
-      name: 'João de Deus',
-      email: 'jdeus@escola.com.br',
-      created_at: '2025-10-28T23:50:40.550Z',
-      updated_at: '2025-10-28T23:50:40.550Z',
-    },
-  })
-  @ApiResponse({
-    status: 404,
-    description: 'Aluno não encontrado.',
-    example: {
-      statusCode: 404,
-      timestamp: '2025-10-29T19:13:47.142Z',
-      path: '/students/65978',
-      errors: ['O recurso solicitado não foi encontrado.'],
-    },
-  })
+  @ApiFindOneStudent()
   findOne(@Param('ra') ra: string) {
     return this.studentsService.findOne(ra);
   }
 
   @Patch(':ra')
-  @ApiOperation({ summary: 'Atualiza um aluno pelo RA' })
-  @ApiResponse({
-    status: 200,
-    description: 'Aluno atualizado com sucesso.',
-    example: {
-      ra: '6597',
-      cpf: '12345678985',
-      name: 'João de Jesus',
-      email: 'jdeus@escola.com.br',
-      created_at: '2025-10-28T23:50:40.550Z',
-      updated_at: '2025-10-28T23:52:24.550Z',
-    },
-  })
-  @ApiResponse({
-    status: 404,
-    description: 'Aluno não encontrado.',
-    example: {
-      statusCode: 404,
-      timestamp: '2025-10-29T19:13:47.142Z',
-      path: '/students/65978',
-      errors: ['O recurso solicitado não foi encontrado.'],
-    },
-  })
+  @ApiUpdateStudent()
   update(@Param('ra') ra: string, @Body() updateStudentDto: UpdateStudentDto) {
     return this.studentsService.update(ra, updateStudentDto);
   }
 
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':ra')
-  @ApiOperation({ summary: 'Remove um aluno pelo RA' })
-  @ApiResponse({ status: 204, description: 'Aluno removido com sucesso.' })
-  @ApiResponse({
-    status: 404,
-    description: 'Aluno não encontrado.',
-    example: {
-      statusCode: 404,
-      timestamp: '2025-10-29T19:13:47.142Z',
-      path: '/students/65978',
-      errors: ['O recurso solicitado não foi encontrado.'],
-    },
-  })
+  @ApiRemoveStudent()
   remove(@Param('ra') ra: string) {
     return this.studentsService.remove(ra);
   }
